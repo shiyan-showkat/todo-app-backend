e22de232846ea9dd50e0ae7bcec023c2f341c21f
shiyan <shiyanshowkat93@gmail.com>
2026-04-18 12:31:51 +0530

    changes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: " https://todo-app-backend-gfh3.onrender.com",
+    origin: "https://todo-app-frontend-omega-mauve.vercel.app",
     credentials: true,
   }),
 );
